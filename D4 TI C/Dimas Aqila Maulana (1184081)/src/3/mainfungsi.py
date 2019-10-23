c03f0f957ab9f619e41601b6d72de5d51fcf1b1f
dimas <dimas.maulana370@gmail.com>
2019-10-24 00:37:33 +0700

    Tugas Pemrograman 2 Chapter 1

---
+++ D4 TI C/Dimas Aqila Maulana (1184081)/src/3/mainfungsi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 06:07:42 2019
+
+@author: awangga
+"""
+import kalkulator
+
+a=100
+b=50
+
+hasil1=kalkulator.Penambahan(a,b)
+hasil2=kalkulator.Pengurangan(a,b)
+hasil3=kalkulator.Perkalian(a,b)
+hasil4=kalkulator.Pembagian(a,b)
