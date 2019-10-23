ab256a9ecc2a7c75a0a70a62b6b5fb9115445b44
MFarhanFadl <52203036+MFarhanFadl@users.noreply.github.com>
2019-10-24 01:28:02 +0700

    Add files via upload
    DEspacito 2 confirm by nasa

---
+++ Chapter 2/D4 TI B/M. Farhan Fadlurrahman (1184072)/code/N3.py
@@ -0,0 +1,13 @@
+pms = "072"
+npm = 1184072
+jumlah = 0+7+2
+
+
+print ("0 + 7 + 2 = ", jumlah)
+
+print ("input : ", npm)
+
+print ("output : ")
+
+for i in range (jumlah) :
+    print ("Hello, ",pms, "apa kabar ?")
