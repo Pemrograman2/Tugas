a7421d2f04914289022d7214795bef8f9c7da8ae
Akil <itsmeakil707@gmail.com>
2019-10-24 01:01:10 +0700

    Akil Munawwar (1184041)

---
+++ Chapter 2/D4 TI B/Akil Munawwar (1184041)/Src/Pertambahan.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 09:19:43 2019
+
+@author: Asus
+"""
+a = 2
+b = 3
+
+c = a + b
+print (c)
+
+c = a-b
+print (c)
+
+c = a/b
+print (c)
+
+c = a*b
+print (c)
---
+++ Chapter 2/D4 TI B/Akil Munawwar (1184041)/Src/PerulanganWhile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 13:41:53 2019
+
+@author: Asus
+"""
+
+jawab = 'ya'
+hitung = 0
+
+while(jawab == 'ya'):
+    hitung += 1
+    jawab = input("Ulang lagi tidak? ")
+
+print ("Total perulagan: " + str(hitung))
