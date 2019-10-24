c1d7fe5202f74a8b1532220a0d925319e2f615a9
Etika Khusnul Laeli <44889084+1184065@users.noreply.github.com>
2019-10-24 23:48:40 +0700

    Add files via upload

---
+++ Etika Khusnul Laeli (1184065)/src/NPM4.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 18:55:30 2019
+
+@author: ANIF
+"""
+NPM=int(input("masukan NPM :"))
+    key=npm%1000
+    str_key=str(key)
+    print("Hakko, "+str_key[0]+" apa kabar?")
