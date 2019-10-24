e81f57246198fe99608d2960af2a84d6188e7f67
Gharrieb <56650080+Gharrieb@users.noreply.github.com>
2019-10-24 14:13:45 +0700

    Add files via upload

---
+++ Chapter 2/D4 TI B/Gharrieb Nauvaldi (1184082)/code/temp2.py
@@ -0,0 +1,8 @@
+NPM = 1184082
+
+print ("input : ", NPM)
+
+print ("output : ")
+
+for i in range (82) :
+    print ("Hello, ",NPM, "apa kabar ?")
---
+++ Chapter 2/D4 TI B/Gharrieb Nauvaldi (1184082)/code/temp4.py
@@ -0,0 +1,8 @@
+NPM = 1184082
+
+print ("input : ", NPM)
+
+print ("output : ")
+
+for i in range (1) :
+    print ("Hello, ",0, "apa kabar ?")
