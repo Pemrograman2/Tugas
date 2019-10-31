ca8e5021c100f3e2edc80eda4aba3434f34e8ac1
aditya <mas.adityarahman@gmail.com>
2019-10-31 15:32:45 +0700

    Punya adit

---
+++ Chapter 3/D4 TI B/Aditya Rahman (1184021)/src/src/lib3.py
@@ -0,0 +1,108 @@
+def npm1():
+    print("***   ***   *******   ******  ****** ******  ***")
+    print("***   ***   **   **  **   **  **  **     **  ***")
+    print("***   ***    ****   ********  **  ** ******  ***")
+    print("***   ***   **   **       **  **  ** **      ***")
+    print("***   ***   *******       **  ****** ******  ***")
+def npm2(npm):
+    npm=int(npm)
+    TwoLastDigit=abs(npm)%100
+    for i in range(TwoLastDigit):
+       print("Halo, ", npm, " apa kabar ?")
+def npm3(npm):
+    for i in range(int(str(npm)[4])+int(str(npm)[5])+int(str(npm)[6])):
+        print("Halo, "+str(npm)[4]+str(npm)[5]+str(npm)[6]+" apa kabar ?")
+    return None
+def npm4(npm):
+    key=npm%1000
+    str_key=str(key)
+    print("Halo, "+str_key[0]+" apa kabar ?")
+def npm5(npm):
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+
+    for x in a,b,c,d,e,f,g:
+        print(x)
+
+def npm6(npm):
+
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    y=0
+
+    for x in a,b,c,d,e,f,g:
+        y+=int(x)
+    print(y)
+def npm7(npm):
+
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    conv=1
+
+    for x in a,b,c,d,e,f,g:
+        conv*=int(x)
+    print(conv)
+def npm8(npm):
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    for x in a,b,c,d,e,f,g:
+        if int(x)%2==0:
+            if int(x)==0:
+                x=""
+            print(x,end ="")
+def npm9(npm):
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    for x in a,b,c,d,e,f,g:
+    
+        if int(x)%2==1:
+            print(x,end ="")
+def npm10(npm):
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    for x in a,b,c,d,e,f,g:    
+        if int(x) > 1:
+            for i in range(2,int(x)):
+                if (int(x) % i) == 0:
+                    break
+            else:
+                print(int(x),end =""),
+
+
+
+
+
+
+
+
+
---
+++ Chapter 3/D4 TI B/Aditya Rahman (1184021)/src/src/main.py
@@ -0,0 +1,28 @@
+import kelas3lib
+import lib3
+from kalkulator import penambahan
+    
+
+
+npm=input("Masukan NPM kalian : ")
+i=0
+while i<1:
+    if len(npm) < 7:
+        print("NPM Kurang dari 7 digit")
+        npm=input("Masukan NPM kalian : ")
+    elif len(npm) > 7:
+        print("NPM lebih dari 7 digit")
+        npm=input("Masukan NPM kalian : ")
+    else:
+        i=1
+
+#Contoh pemanggilan fungsi pada class
+cobakelas=kelas3lib.Kelas3ngitung(npm) 
+hasilkelas=cobakelas.npm1()
+
+
+print("")
+
+#Contoh pemanggilan fungsi pada library
+lib3.npm3(npm)
+print(penambahan.tambah(1,2))
