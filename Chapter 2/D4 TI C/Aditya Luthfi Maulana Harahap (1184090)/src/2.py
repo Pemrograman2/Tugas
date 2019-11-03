4c834a541174f7406563b8770b74099ea959fcc7
adityaluthfimaulanaharahap <aditluthfi21@gmail.com>
2019-11-03 22:42:39 +0700

    Aditya Luthfi Maulana Harahap(1184090)

---
+++ Chapter 2/D4 TI C/Aditya Luthfi Maulana Harahap (1184090)/src/2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 14:17:39 2019
+
+@author: Aditya Luthfi
+"""
+
+npm = input("masukan npm :")
+key = int(npm)%10
+for i in range(key):
+        print("Halo",npm,"Apa Kabar?")
---
+++ Chapter 2/D4 TI C/Aditya Luthfi Maulana Harahap (1184090)/src/4.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 14:52:47 2019
+
+@author: Aditya Luthfi
+"""
+
+npm = input("Masukan NPM :")
+
+print("input :",npm)
+print("output :",npm)
+print("halo", npm[4], "apa kabar?")
