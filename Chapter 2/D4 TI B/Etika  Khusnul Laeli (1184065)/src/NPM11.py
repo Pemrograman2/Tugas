f4eb2f9155bf4a8162fb9f76c2ed9d99e2eb9ecb
@EtikaKL <etikalaeli@gmail.com>
2019-11-04 14:45:47 +0700

    update

---
+++ Chapter 2/D4 TI B/Etika  Khusnul Laeli (1184065)/src/NPM11.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 22:15:17 2019
+
+@author: ANIF
+"""
+
+#DigitPrima
+npm=input("Masukan NPM : ")
+a=npm[0]
+b=npm[1]
+c=npm[2]
+d=npm[3]
+e=npm[4]
+f=npm[5]
+g=npm[6]
+y=0
+
+for x in b:
+    print(str(a) + str(b) + str(g))
