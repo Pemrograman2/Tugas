48606cef2ed47aca29b1afd42c690ae590370e66
trianggadios <trianggadio@gmail.com>
2019-11-22 06:46:42 +0700

    initial commit

---
+++ Chapter 3/D4 TI B/Tri Angga Dio Simamora(1184047)/src/main.py
@@ -0,0 +1,17 @@
+from kode_tugas import Tugas
+
+# npm = input("Masukkan NPM : ")
+#
+# run = Tugas(npm)
+
+# run.nomor1()
+# run.nomor2()
+# run.nomor3()
+# run.nomor4()
+# run.nomor5()
+# run.nomor6()
+# run.nomor7()
+# run.nomor8()
+# run.nomor9()
+# run.nomor10()
+# run.nomor11()
