1e31e0b5cf6a269221c26b4c5ae2714784ba7cd6
Alvian2109 <alviandaniel2109@gmail.com>
2019-11-24 01:46:29 +0700

    Push Chapter 3

---
+++ Chapter 3/D4 TI C/Alvian Daniel Sinaga (1184077)/src/NPM10.py
@@ -0,0 +1,29 @@
+def npm10(npm):
+    a=npm[0]
+    b=npm[1]
+    c=npm[2]
+    d=npm[3]
+    e=npm[4]
+    f=npm[5]
+    g=npm[6]
+    for x in a,b,c,d,e,f,g:    
+        if int(x) > 1:
+            for i in range(2,int(x)):
+                if (int(x) % i) == 0:
+                    break
+            else:
+                print(int(x),end =""),
+
+i=0
+npm=input("Masukan NPM : ")
+
+while i<1:
+    if len(npm) < 7:
+        print("NPM Kurang dari 7 digit")
+        npm=input("Masukan NPM : ")
+    elif len(npm) > 7:
+        print("NPM lebih dari 7 digit")
+        npm=input("Masukan NPM : ")
+    else:
+        i=1
+npm10(npm)
