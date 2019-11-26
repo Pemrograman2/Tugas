9d465d33b5d0d57bc36d7021ccffecefa6de15f9
Veldi Alhasib <veldialhasib998@gmail.com>
2019-11-27 00:54:05 +0700

    Chapter2

---
+++ Chapter 2/D4 TI B/Veldi (1184055)/src/temp3.py
@@ -0,0 +1,12 @@
+npm = 1184055
+jumlah = 0+5+5
+pms = "055"
+
+print ("0 + 5 + 5 = ", jumlah)
+
+print (" input : ", npm)
+
+print ("output : ")
+
+for i in range (jumlah) :
+    print ("Hello, ", npm, "apa kabar ?")
---
+++ Chapter 2/D4 TI B/Veldi (1184055)/src/temp4.py
@@ -0,0 +1,8 @@
+NPM = 1184055
+
+print ("input : ", NPM)
+
+print ("output : ")
+
+for i in range (1) :
+    print ("Hello, ",0, "apa kabar ?")
